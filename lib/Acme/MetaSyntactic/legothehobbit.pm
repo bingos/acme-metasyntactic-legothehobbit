fd6b868b5a4f9a13f7ad58d04a6fd436afc6c293
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2014-10-21 17:41:22 +0100

    Initial release

---
+++ lib/Acme/MetaSyntactic/legothehobbit.pm
@@ -0,0 +1,107 @@
+package Acme::MetaSyntactic::legothehobbit;
+
+#ABSTRACT: LEGO The Hobbit game theme
+
+use utf8;
+use strict;
+use warnings;
+use Acme::MetaSyntactic::List;
+our @ISA = qw( Acme::MetaSyntactic::List );
+__PACKAGE__->init();
+
+q[In a hole in the ground there lived a hobbit];
+
+=pod
+
+=encoding utf8
+
+=head1 DESCRIPTION
+
+Characters from the LEGO The Hobbit game as gleaned from
+the L<http://www.gamefaqs.com/> walkthroughs.
+
+I<In a hole in the ground there lived a hobbit>
+
+=head1 SEE ALSO
+
+L<Acme::MetaSyntactic>, L<Acme::MetaSyntactic::List>.
+
+=cut
+
+__DATA__
+# names
+Thorin
+Thrain
+Balin
+Thror
+Armoured_Dwarf_Guard
+Frodo
+Sam
+Bilbo
+Gandalf
+Bifur
+Bofur
+Bombur
+Dori
+Dwalin
+Fili
+Kili
+Gloin
+Nori
+Oin
+Ori
+Radagast
+Goblin_Confused
+Legolas_Greenleaf
+Tauriel
+Bombur_Barrel
+Bard
+Tom_Bombadil
+Gundabad_Orc
+Barrow_wight
+Saruman
+Elrond_Gown
+Elrond_Armoured
+Lindir
+Yazneg
+Goblin_Scribe
+Grinnah
+Gollum
+Barliman_Butterbur
+Beorn
+Mirkwood_Elf_Archer
+Mirkwood_Elf_Guard
+Mirkwood_Elf_Chief_Guard
+Elf_Sentry
+Elf_Worker
+Bolg
+Thranduil
+Narzug
+Elros
+Master_of_Lake_town
+Alfrid
+Braga
+Lake_town_Man_Guard
+Lake_town_Man_Archer
+Percy
+Tilda
+Sigrid
+Bain
+Fimbul
+Witch_king
+Azog_Claw
+Necromancer
+Sauron
+Dale_Soldier
+Galadriel
+Rosie_Cotton
+Bree_Peasant
+Jimli_the_Blacksmith
+Orc_Berserker
+The_Great_Goblin
+Goblin_Brute
+Bert_Troll
+Tom_Troll
+William_Troll
+Mrs_Troll
+Troll_Bouncer
